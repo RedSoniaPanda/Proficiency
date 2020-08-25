95f3c349a579aa82cc6a46c6483f2e77fce1c35d
Sophia Farr <sophia.farr.427@gmail.com>
2020-08-25 13:41:58 -0700

    Added feature to find the last two of the LLL more than once previously in the list a

--- main.cpp
+++ main.cpp
@@ -10,14 +10,26 @@ void display_proficiency_version(int argc, char *const *argv);
 
 int main(int argc, char *argv[]) {
     std::cout << "Hello, World!" << std::endl;
-
     display_proficiency_version(argc, argv);
-
     List linear_linked_list = build_list();
+
     int sum;
+    bool is_found = true;
+//    linear_linked_list.insert_at_end_of_lll(2);
+//    linear_linked_list.insert_at_end_of_lll(2);
+//    linear_linked_list.insert_at_end_of_lll(2);
+
+    is_found = linear_linked_list.is_last_two_found_more_than_once();
+    if (is_found) {
+        cout << "Found last two data points more than once" << endl;
+    }
+    else {
+        cout << "Didn't find last two data points more than once" << endl;
+    }
 
     linear_linked_list.insert_at_end_of_lll(0);
     linear_linked_list.display_all();
+
     linear_linked_list.display_not_equal_head();
 
     sum = linear_linked_list.sum_larger_than_tail();
@@ -49,5 +61,6 @@ List build_list() {
         ++value;
     }
 
+    linear_linked_list.display_all();
     return linear_linked_list;
 }
