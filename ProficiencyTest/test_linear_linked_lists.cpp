ab0c9fa0c88e6b6e367b54e178855ccbbe87c32e
Sophia Farr <sophia.farr.427@gmail.com>
2020-08-27 10:59:51 -0700

    Add Google Test Framework to continue unit testing

--- ProficiencyTest/test_linear_linked_lists.cpp
+++ ProficiencyTest/test_linear_linked_lists.cpp
@@ -9,7 +9,14 @@ TEST(TestingEnvironment, TestHarness) {
     EXPECT_EQ(0, 0);
 }
 
-TEST(TestLLL, TestCreateClass) {
+class TestList : public ::testing::Test {
+protected:
+    void SetUp() override {}
+public:
     List new_linear_linked_list;
-    EXPECT_EQ(sizeof(new_linear_linked_list), 0);
+};
+
+TEST_F(TestList, TestInsertOneAtTail) {
+    new_linear_linked_list.insert_at_end_of_lll(1);
+    EXPECT_EQ(sizeof(new_linear_linked_list), 1);
 }
